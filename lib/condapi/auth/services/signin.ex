e6ce19351afe5c208508828488d0374e815df01d
Carlos Soteras <carlosoteras@gmail.com>
2023-01-06 11:53:30 -0300

    Add downcase on fetch_by.

--- lib/condapi/auth/services/signin.ex
+++ lib/condapi/auth/services/signin.ex
@@ -9,7 +9,7 @@ defmodule Condapi.Auth.Services.Signin do
   alias Condapi.Auth.Io.Repo.User, as: UserRepo
 
   def process(%{email: email, password: password}) do
-    with {:ok, user} <- UserRepo.fetch_by(email: email),
+    with {:ok, user} <- UserRepo.fetch_by(email: String.downcase(email)),
          true <- Bcrypt.verify_pass(password, user.password_hash),
          {:ok, token, _claims} <- Guardian.encode_and_sign(user) do
       {:ok, %{token: token}}
